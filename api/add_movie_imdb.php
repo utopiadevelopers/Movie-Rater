5d64648403db9ac5697d2e683bbe03f76f9e24dd
Satyam Krishna <satyamkrishna92@gmail.com>
2014-10-24 22:16:00 +0530

    Minor Rank Changes

--- api/add_movie_imdb.php
+++ api/add_movie_imdb.php
@@ -12,13 +12,13 @@ $data = $db->ud_mysql_fetch_assoc_all($results);
 
 if(sizeof($data)>0)
 {
-	$db->ud_updateQuery('movie',array('name'=>$_POST['name'],'year'=>$_POST['year'],'rating'=>$_POST['rating'],'imdb_id'=>$_POST['imdb_id'],'imdb_rating'=>$_POST['imdb_id']),array('imdb_id'=>$_POST['imdb_id']));	
+	$db->ud_updateQuery('movie',array('name'=>$_POST['name'],'year'=>$_POST['year'],'rating'=>$_POST['rating'],'imdb_id'=>$_POST['imdb_id'],'imdb_rating'=>$_POST['rating'],'imdb_rank'=>$_POST['imdb_rank']),array('imdb_id'=>$_POST['imdb_id']));	
 	$message['status']="Success";
 	$message['message'] = $_POST['name']." was updated successfully";
 }
 else
 {
-	$db->ud_insertQuery('movie',array('name'=>$_POST['name'],'year'=>$_POST['year'],'rating'=>$_POST['rating'],'imdb_id'=>$_POST['imdb_id'],'imdb_rating'=>$_POST['imdb_id']));
+	$db->ud_insertQuery('movie',array('name'=>$_POST['name'],'year'=>$_POST['year'],'rating'=>$_POST['rating'],'imdb_id'=>$_POST['imdb_id'],'imdb_rating'=>$_POST['rating'],'imdb_rank'=>$_POST['imdb_rank']));
 	$message['status']="Present";
 	$message['message'] = $_POST['name']." was added successfully";
 }
--- python/imdb_grab_250.py
+++ python/imdb_grab_250.py
@@ -5,10 +5,12 @@ import string
 import json
 
 url = 'http://www.imdb.com/chart/top?ref=ft_250'
+#url = 'http://localhost/imdb.php'
 
 response = urllib2.urlopen(url)
 soup = BeautifulSoup(response)
 result = soup.select(".lister-list > tr")
+imdb_rank = 0
 
 for data in result:
     titleColumn = data.find("td",class_="titleColumn")
@@ -25,9 +27,11 @@ for data in result:
     imdbIDField = watchlistColumn.find("div")
     imdbID = imdbIDField['data-tconst']
     
+    imdb_rank = imdb_rank + 1
+    
     # URL To API Folder 
     url = 'http://movie-rater/add_movie_imdb.php'
-    values = {'name' : movie_name.encode('utf8') , 'rating':movie_rating.encode('utf8'),'year':movie_year,'imdb_id':imdbID}
+    values = {'name' : movie_name.encode('utf8') , 'rating':movie_rating.encode('utf8'),'year':movie_year,'imdb_id':imdbID,'imdb_rank':imdb_rank}
     data = urllib.urlencode(values)
     req = urllib2.Request(url, data)
     response = urllib2.urlopen(req)
