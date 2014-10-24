66da3cb29f3104b18569ebd0656d9353861ace1e
Satyam Krishna <satyamkrishna92@gmail.com>
2014-10-24 22:23:57 +0530

    IMDB Bottom 100 Parser

---
+++ python/imdb_grab_bottom_100.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import urllib
+import urllib2
+import string
+import json
+
+url = 'http://www.imdb.com/chart/bottom?ref_=chttp_ql_3'
+#url = 'http://localhost/imdb.php'
+
+response = urllib2.urlopen(url)
+soup = BeautifulSoup(response)
+result = soup.select(".lister-list > tr")
+imdb_rank = 0
+
+for data in result:
+    titleColumn = data.find("td",class_="titleColumn")
+    nameField = titleColumn.find("a")   
+    movie_name = nameField.string
+    yearField = titleColumn.find("span",class_="secondaryInfo")
+    movie_year = yearField['data-value']
+    
+    ratingColumn = data.find("td",class_="ratingColumn")
+    ratingField  = ratingColumn.find("strong")
+    movie_rating = ratingField.string
+    
+    watchlistColumn = data.find("td",class_="watchlistColumn")
+    imdbIDField = watchlistColumn.find("div")
+    imdbID = imdbIDField['data-tconst']
+    
+    imdb_rank = 0
+    
+    # URL To API Folder 
+    url = 'http://movie-rater/add_movie_imdb.php'
+    values = {'name' : movie_name.encode('utf8') , 'rating':movie_rating.encode('utf8'),'year':movie_year,'imdb_id':imdbID,'imdb_rank':imdb_rank}
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    jsonObj = json.loads(response.read())
+    print jsonObj['message']
